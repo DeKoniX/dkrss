b2db3c99c7d2daf1f67a4986b2628cbf4160517a
DeKoniX <dekonix@gmail.com>
2013-10-17 13:55:55 +0700

    Костыль с аундефикацией, бутстрап списки + цвет

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,4 +1,6 @@
 class SitesController < InheritedResources::Base
+  http_basic_authenticate_with name: "admin", password: "qaz12345", except: :index
+
   def create
     create! { sites_path }
   end
