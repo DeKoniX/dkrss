8bf9104848958dec21da05ff7ac886962eef7659
DeKoniX <dekonix@gmail.com>
2013-11-13 22:02:28 +0700

    Сортировка по дате

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,7 +1,7 @@
 class IndexController < ApplicationController
   def index
     if user_signed_in?
-      @feeds = current_user.feeds.paginate(page: params[:page], order: "created_at DESC", per_page: 10)
+      @feeds = current_user.feeds.paginate(page: params[:page], order: "date DESC", per_page: 10)
       if @feeds == []
         redirect_to sites_path
       end
