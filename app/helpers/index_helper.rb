9c1354d545318e9840dd7271ea6307213f21a398
DeKoniX <dekonix@gmail.com>
2016-06-20 06:49:54 +0700

    Ошибка, когда description получен как nil

--- app/helpers/index_helper.rb
+++ app/helpers/index_helper.rb
@@ -1,9 +1,11 @@
 module IndexHelper
   def description_l(description)
-    if description.length > 2000
-      description[0..2000] 
-    else
-      description
+    unless description.nil?
+      if description.length > 2000
+        description[0..2000] 
+      else
+        description
+      end
     end
   end
 end
