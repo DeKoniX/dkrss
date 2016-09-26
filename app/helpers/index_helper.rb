29b5040c66068f2486e0604156b2e74c624552b5
DeKoniX <dekonix@gmail.com>
2016-09-26 23:49:33 +0700

    Уменьшаю описание стилем

--- app/helpers/index_helper.rb
+++ app/helpers/index_helper.rb
@@ -1,11 +1,3 @@
 module IndexHelper
-  def description_l(description)
-    unless description.nil?
-      if description.length > 2000
-        description[0..2000]+' [...]' 
-      else
-        description
-      end
-    end
-  end
+
 end
